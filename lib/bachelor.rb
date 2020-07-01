e2cf0c0831c9a84df7cde87c28ab5937bc001bc0
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-01 14:43:14 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,11 @@
 def get_first_name_of_season_winner(data, season)
-  
+  contestant = data[season].select {|contestant|
+  contestant["status"] == "Winner"}
+  contestant["name"].split(' ')[0]
 end
 
 def get_contestant_name(data, occupation)
-  "Carly Waddell"
+  # code here
 end
 
 def count_contestants_by_hometown(data, hometown)
