a3425a96f620a3a1bd5ea0b79ccd2f602cb3f686
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-01 14:41:18 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,8 +3,7 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  data.each {|season| 
-    name = season[name]season[occupation] == occupation
+  # code here
 end
 
 def count_contestants_by_hometown(data, hometown)
